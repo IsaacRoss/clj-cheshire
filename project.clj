37813d2784cc301a14d207ea4dc431bc1a8b4972
Isaac Ross <isaac253@gmail.com>
2015-12-25 20:04:57 -0500

    initial commit

---
+++ project.clj
@@ -0,0 +1,22 @@
+(defproject chesire-cat "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [compojure "1.4.0"]
+                 [ring/ring-defaults "0.1.5"]
+                 [ring/ring-json "0.4.0"]
+                 [org.clojure/clojurescript "1.7.170"]]
+  :plugins [[lein-ring "0.9.7"]
+            [lein-cljsbuild "1.1.2"]]
+  :ring {:handler chesire-cat.handler/app}
+  :profiles
+  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                        [ring/ring-mock "0.3.0"]]}}
+  :cljsbuild {
+    :builds [{
+        :source-paths ["src-cljs"]
+        :compiler {
+          :output-to "resources/public/main.js"
+          :optimizations :whitespace
+          :pretty-print true}}]})
